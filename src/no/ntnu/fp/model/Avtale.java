e8dc777c4f15b866f6afa3c83d120af82805c764
AviVidi <avividi@dhcp-011136.wlan.ntnu.no>
2011-03-17 11:35:25 +0100

    Gjort om hendelse til avtale og lagt til mote

--- src/no/ntnu/fp/model/Avtale.java
+++ src/no/ntnu/fp/model/Avtale.java
@@ -1,5 +1,41 @@
 package no.ntnu.fp.model;
 
-public class Avtale extends Hendelse {
+import java.util.Date;
 
+public abstract class Avtale {
+
+	private int starttid, sluttid;
+	
+	private Date dato;
+	
+	private String tittel;
+	private String beskrivelse;
+	
+	private Person initiativtaker;
+
+	public String getTittel() {
+		return tittel;
+	}
+
+	public void setTittel(String tittel) {
+		this.tittel = tittel;
+	}
+
+	public String getBeskrivelse() {
+		return beskrivelse;
+	}
+
+	public void setBeskrivelse(String beskrivelse) {
+		this.beskrivelse = beskrivelse;
+	}
+
+	public Person getInitiativtaker() {
+		return initiativtaker;
+	}
+
+	public void setInitiativtaker(Person initiativtaker) {
+		this.initiativtaker = initiativtaker;
+	}
+	
+	
 }
