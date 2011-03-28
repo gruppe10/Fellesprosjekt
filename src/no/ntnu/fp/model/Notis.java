5c07d5f4d3c47cbee04e46ed737fe82be60a8c12
unknown <Kristin@.(none)>
2011-03-28 15:03:33 +0200

    Opprettet og endret Mote, Notis, Avtale og Rom

---
+++ src/no/ntnu/fp/model/Notis.java
@@ -0,0 +1,19 @@
+package no.ntnu.fp.model;
+
+public class Notis {
+	
+	private Person mottaker;
+	private Mote mote;
+	private String beskjed;	
+	
+	final String ENDRET = "Møtet er endret";
+	final String SLETTET = "Møtet er slettet";
+	final String MELDTAVBUD ="Deltaker har meldt avbud";
+	
+	public Notis(Person mottaker, Mote mote, String beskjed){
+		this.mottaker = mottaker;
+		this.mote = mote;
+		this.beskjed = beskjed;
+	}
+
+}
