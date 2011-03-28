b13df7c08c646fd91aa394cce1789c0e635b3b11
unknown <Kristin@.(none)>
2011-03-28 15:57:34 +0200

    lagt til Rom

---
+++ src/no/ntnu/fp/model/Rom.java
@@ -0,0 +1,43 @@
+package no.ntnu.fp.model;
+
+public class Rom {
+	
+	private int romId;
+	private String navn;
+	private int str;
+	
+	public int getStr() {
+		return str;
+	}
+
+	public void setStr(int str) {
+		this.str = str;
+	}
+
+	public Rom(String navn){
+		this.navn = navn;
+//		Hent den nye id'en fra databasen (autogenereres der)
+	}
+
+	public int getRomId() {
+		return romId;
+	}
+
+	public void setRomId(int romId) {
+		this.romId = romId;
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+	
+	public boolean checkLedig(String tid){
+		return (//spørring mot database om et annet møte bruker rommet) ? false : true ;
+	}
+	
+	
+}
