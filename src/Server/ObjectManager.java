2b5074485bd06f656eecc9d2ca021c86279feb05
martin <gruppe10eier@gmail.com>
2011-04-01 13:38:47 +0100

    Updated ObjectManager

--- src/Server/ObjectManager.java
+++ src/Server/ObjectManager.java
@@ -1,5 +1,10 @@
 package Server;
 
+import java.sql.SQLException;
+
+import Klient.Action;
+import Klient.Envelope;
+
 import no.ntnu.fp.model.Avtale;
 import no.ntnu.fp.model.Hendelse;
 import no.ntnu.fp.model.Mote;
@@ -7,29 +12,41 @@ import no.ntnu.fp.model.Notis;
 import no.ntnu.fp.model.Person;
 import no.ntnu.fp.model.Project;
 import no.ntnu.fp.model.Rom;
+import no.ntnu.fp.model.record.*;
 
 public class ObjectManager {
 
 	public Object manageObject(Object o) {
-		// TODO Auto-generated method stub
-		if( o instanceof Notis){
-			//kode for håndtering av sending av notis til db
-			System.out.println("notisok");
-			return null;
-		}
-		else if( o instanceof Avtale){
+		Envelope e = (Envelope)o;
+		
+		Object content = e.getContent();
+		Action action = e.getAction();
+	
+		if( content instanceof Avtale){
+			Avtale avtale =(Avtale)content;
+			switch(action){
+				case UPDATE:
+					
+					break;
+				case DESTROY:
+					ActiveAvtale.deleteAvtale(avtale.getAvtaleId());
+					break;
+			}
 			System.out.println("avtale");
 			return null;
 		}
-		else if( o instanceof Mote){
+		else if( content instanceof Mote){
 			System.out.println("moete");
 			return null;
 		}
-		else if( o instanceof Person){
+		else if( content instanceof Person){
+
+			ActivePerson.createPerson((Person)o);
+			
 			System.out.println("person");
 			return null;
 		}
-		else if( o instanceof Rom){
+		else if( content instanceof Rom){
 			System.out.println("rom");
 			return null;
 		}
