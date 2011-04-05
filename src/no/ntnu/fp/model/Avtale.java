c5a74d71eb88150e320e63d74db12321fc489031
AviVidi <avividi@dhcp-002237.wlan.ntnu.no>
2011-04-05 16:26:09 +0200

    fix

--- src/no/ntnu/fp/model/Avtale.java
+++ src/no/ntnu/fp/model/Avtale.java
@@ -34,8 +34,10 @@ public class Avtale implements Serializable{
 		this.datoMnd = datoMnd;
 		this.datoAar = datoAar;
 		this.rom = rom;
-		//KlientOS klient = KlientOS.getInstance();
-		//klient.sendObjectAndGetResponse(this);
+		if (!(this instanceof Mote)) {
+			KlientOS klient = KlientOS.getInstance();
+			klient.sendObjectAndGetResponse(this);
+		}
 		
 	}
 	public Avtale(int id, String navn, String beskrivelse, Person initiativtaker, int starttid, int sluttid, int datoDag, int datoMnd, int datoAar, Object rom){
@@ -48,8 +50,10 @@ public class Avtale implements Serializable{
 		this.datoDag = datoDag;
 		this.datoMnd = datoMnd;
 		this.datoAar = datoAar;
-		KlientOS klient = KlientOS.getInstance();
-		klient.sendObjectAndGetResponse(this);
+		if (!(this instanceof Mote)) {
+			KlientOS klient = KlientOS.getInstance();
+			klient.sendObjectAndGetResponse(this);
+		}
 	}
 	public Avtale(){
 		starttid = sluttid = 0;
@@ -160,8 +164,10 @@ public class Avtale implements Serializable{
 		this.navn = navn;
 		this.beskrivelse = beskrivelse;
 		this.rom = moterom;
-		KlientOS klient = KlientOS.getInstance();
-		klient.sendObjectAndGetResponse(this);
+		if (!(this instanceof Mote)) {
+			KlientOS klient = KlientOS.getInstance();
+			klient.sendObjectAndGetResponse(this);
+		}
 		
 	}
 
