45a77282db206b917c3f6849b862bdcf94cad669
Audun <Audun@.(none)>
2011-04-05 13:42:15 +0200

    La til javadoc i KlientOS og fikser KlientOS-kode i Avtale og Mote

--- src/Klient/KlientOS.java
+++ src/Klient/KlientOS.java
@@ -10,23 +10,36 @@ import java.util.TimerTask;
 import no.ntnu.fp.model.Avtale;
 import no.ntnu.fp.model.Person;
 
+/**
+ * Brukes for å koble til databasen, og sende og motta objekter wrappet i en {@code Envelope}
+ * 
+ * Hent ut en instans av klassen ved å bruke {@code KlientOS.getInstance()}
+ * Denne metoden vil opprette et objekt av KlientOS om det ikke finnes fra før, om det finnes
+ * vil det returnere det allerede opprettede objektet.
+ * 
+ * {@code intervalUpdate} sørger for at {@code run()}-metoden henter et nytt personobjekt gitt brukernavnet i {@code loginPerson}
+ * 
+ * 
+ */
 public class KlientOS extends TimerTask{
-	
+	//Endre port og adresse til serveren her.
 	public static int port = 6789;
 	public static String serverAddress = "78.91.2.252";
 	
 	
-	public Person updatedPerson = null;
+	
+	private Person updatedPerson = null;
 	private Person loginPerson = null;
 	private Socket socket = null;
 	private ObjectInputStream ois;
 	private ObjectOutputStream oos;
 	private Timer intervalUpdate = new Timer(true);
     private static final KlientOS INSTANCE = new KlientOS();
+    private int timeInterval = 100000;
 
 	
 	private KlientOS(){
-		intervalUpdate.schedule(this, 100, 100000);
+		intervalUpdate.schedule(this, 100, timeInterval);
 	}
 	public static KlientOS getInstance(){
 		return INSTANCE;
