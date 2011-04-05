8da014166f4877d4eb564f2ef9f29b949306b9e0
martin <gruppe10eier@gmail.com>
2011-04-05 17:15:31 +0200

    refactored ActiveModel

--- src/no/ntnu/fp/model/record/CreateDb.java
+++ src/no/ntnu/fp/model/record/CreateDb.java
@@ -1,9 +1,67 @@
 package no.ntnu.fp.model.record;
 
-public class CreateDb {
+import java.sql.Statement;
+
+public class CreateDb extends ActiveModel{
 	public static void main(String[] args){
-		ActiveModel.createDB();
+		createDB();
 		System.out.println("doom, doom, doom, done!");
-		
+	}
+
+	protected static void createDB(){
+		try{
+			connect();
+			Statement s = connection.createStatement();
+
+			s.execute(
+					"CREATE TABLE Person( " +
+					"ansattId INT PRIMARY KEY NOT NULL," +
+					"navn VARCHAR(50) NOT NULL," +
+					"brukernavn VARCHAR(50) NOT NULL," +
+					"passord VARCHAR(50) NOT NULL" +
+					")"
+			);
+			s.execute(
+					"CREATE TABLE Hendelse("+
+					"hendelseId INT PRIMARY KEY NOT NULL ,"+
+					"starttid TIME NOT NULL,"+
+					"sluttid TIME NOT NULL,"+
+					"dato DATE NOT NULL,"+
+					"navn VARCHAR(50) NOT NULL,"+
+					"beskrivelse VARCHAR(300),"+
+					"lederId INT," +
+					"FOREIGN KEY (lederId) REFERENCES Person(ansattId)" +
+					")"
+			);
+			s.execute(
+					"CREATE TABLE Deltakere ( "+
+					"ansattId INT,"+
+					"hendelseId INT,"+
+					"status VARCHAR(15) NOT NULL DEFAULT 'IKKE_MOTTATT',"+
+					"PRIMARY KEY (ansattId, hendelseId),"+
+					"FOREIGN KEY (ansattId) REFERENCES Person(ansattId),"+
+					"FOREIGN KEY (hendelseId) REFERENCES Hendelse(hendelseId) ON DELETE CASCADE" +
+					")"
+			);
+			s.execute(
+					"CREATE TABLE Rom ("+
+					"romId INT PRIMARY KEY NOT NULL ,"+
+					"navn VARCHAR(50) NOT NULL" +
+					")"
+			);
+			s.execute(
+					"CREATE TABLE ReserverteRom ("+
+					"romId INT,"+
+					"hendelseId INT,"+
+					"PRIMARY KEY (romId, hendelseId),"+
+					"FOREIGN KEY(romId) REFERENCES Rom(romId),"+
+					"FOREIGN KEY(hendelseId) REFERENCES Hendelse(hendelseId) ON DELETE CASCADE" +
+					")"
+			);
+			connection.close();
+		}catch(Exception e ){
+			System.out.println("Fikk ikke laget db.");
+			System.out.println("Fordi " + e.getMessage());
+		}
 	}
 }
