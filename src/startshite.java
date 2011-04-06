ed46c5631daed69299ab313dec91ab5bc20c910a
martin <gruppe10eier@gmail.com>
2011-04-06 09:35:08 +0200

    llols

--- src/startshite.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.server.ServerCloneException;
-
-
-public class startshite {
-	public static void main(String[] args){
-		no.ntnu.fp.gui.innlogging.main(null);
-		Server.TestServerOS.main(null);
-	}
-}
