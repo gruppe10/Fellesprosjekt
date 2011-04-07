501a3134384f11e3595a47cfc43f4e96b5e48ccd
AviVidi <avividi@dhcp-001157.wlan.ntnu.no>
2011-04-07 11:57:19 +0200

    div impl

--- src/no/ntnu/fp/gui/KalPanInfoBoks.java
+++ src/no/ntnu/fp/gui/KalPanInfoBoks.java
@@ -2,14 +2,23 @@ package no.ntnu.fp.gui;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
+
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.Map;
 
 import no.ntnu.fp.model.Avtale;
 import no.ntnu.fp.model.Mote;
+import no.ntnu.fp.model.Notis;
+import no.ntnu.fp.model.Person;
+import no.ntnu.fp.model.Status;
 
 
 
@@ -19,7 +28,9 @@ public class KalPanInfoBoks extends JPanel implements ActionListener{
 	private JButton endre, slett, avbud;
 	private Avtale a;
 	private kal kal;
-	
+	private JList deltakerList, statusList;
+	private  JScrollPane deltakerPane, statusPane;
+	private DefaultListModel deltakerListModel, statusListModel;
 	public KalPanInfoBoks(kal kal){
 		super();
 		this.kal=kal;
@@ -44,6 +55,25 @@ public class KalPanInfoBoks extends JPanel implements ActionListener{
 		slett.addActionListener(this);
 		slett.hide();
 		add(slett);
+		
+		deltakerPane = new JScrollPane();
+		deltakerListModel = new DefaultListModel();
+		deltakerList = new JList();
+		deltakerPane.setViewportView(deltakerList);
+		deltakerList.setModel(deltakerListModel);
+		//deltakerList.setEnabled(false);
+		deltakerPane.setVisible(false);
+		add(deltakerPane);
+		
+		statusPane = new JScrollPane();
+		statusListModel = new DefaultListModel();
+		statusList = new JList();
+		statusPane.setViewportView(statusList);
+		statusList.setModel(statusListModel);
+		//statusList.setEnabled(false);
+		statusPane.setVisible(false);
+		add(statusPane);
+		
 	}
 	
 	public static void main(String[] args) {
@@ -55,13 +85,41 @@ public class KalPanInfoBoks extends JPanel implements ActionListener{
 	}
 	
 	public void displayAvtale(Avtale a) {
+		if (a instanceof KalPanMoteFiller) {
+			this.a=((KalPanMoteFiller)a).getAvtale();
+		}
+		else if (a instanceof KalPanAvtaleFiller) {	
+			this.a=((KalPanAvtaleFiller)a).getAvtale();
+		}
+		
 		this.a=a;
 		avtaleNavn.setText(a.getNavn()+":  ");
 		avtaleBeskrivelse.setText(a.getBeskrivelse());
 		endre.setVisible(true);
 		slett.setVisible(true);
+		statusPane.setVisible(false);
+		deltakerPane.setVisible(false);
+		
 		
 		if (a instanceof Mote) {
+
+			Map<Person, Status> deltakereMedStatus = ((Mote)a).getDeltakere();
+			
+			for (Map.Entry<Person,Status> e : deltakereMedStatus.entrySet()) {  
+				statusPane.setVisible(true);
+				deltakerPane.setVisible(true);
+				deltakerListModel.clear();
+				statusListModel.clear();
+				
+				
+				Person key = e.getKey();  
+			    Status value = e.getValue(); 
+			    
+			    deltakerListModel.addElement(key);
+			    statusListModel.addElement(value);
+			}  
+			
+			
 			if (kal.getConnectedPerson()==a.getInitiativtaker()) {
 				}
 			else {
@@ -77,6 +135,8 @@ public class KalPanInfoBoks extends JPanel implements ActionListener{
 		slett.setVisible(false);
 		endre.setVisible(false);
 		avbud.setVisible(false);
+		statusPane.setVisible(false);
+		deltakerPane.setVisible(false);
 		
 	}
 	
