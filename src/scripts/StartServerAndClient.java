5448c39f15ef2bf15e3738b9f0c034f6aaa908de
martin <gruppe10eier@gmail.com>
2011-04-08 10:17:58 +0200

    Lal

--- src/scripts/StartServerAndClient.java
+++ src/scripts/StartServerAndClient.java
@@ -5,6 +5,6 @@ import java.rmi.server.ServerCloneException;
 public class StartServerAndClient {
 	public static void main(String[] args){
 		no.ntnu.fp.gui.innlogging.main(null);
-		Server.TestServerOS.main(null);
+		Server.StartServerOS.main(null);
 	}
 }
